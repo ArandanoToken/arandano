39242e468d5655997cf1f459a5039ed69b018412
ArandanoToken <100032280+ArandanoToken@users.noreply.github.com>
2022-03-01 08:44:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ['Reserve", "Owner", "Team", "Holders", "Marketing", "Artist'],
+        labels: ['Owner", "Reserve", "Team", "Holders", "Marketing", "Pools'],
         datasets: [{
-            data: [27.92, 17.53, 14.94, 26.62, 12.99],
+            data: [,, , ,,],
             backgroundColor: ['#e91e63', '#00e676', '#ff5722', '#1e88e5', '#ffd600'],
             borderWidth: 0.5 ,
             borderColor: '#ddd'
@@ -32,14 +32,7 @@ var myChart = new Chart(ctx, {
         tooltips: {
             enabled: false
         },
-        plugins: {
-            datalabels: {
-                color: '#fff',
-                textAlign: 'center',
-                font: {
-                    lineHeight: 1.6
-                    
-                },
+ 
                 formatter: function(value, ctx) {
                     return ctx.chart.data.labels[ctx.dataIndex] + '\n' + value + '%';
                 }
